e64f9e50427c3d30347d6303cdf5fb23c6f66dd9
Atelier Ferrohenge <kai@ferrohenge.com>
2025-11-12 05:06:21 -0800

    Better hand player tracking

--- Hand.cs
+++ Hand.cs
@@ -3,11 +3,4 @@ using System;
 
 public partial class Hand : CardGroup
 {
-	protected CardPlayer _player;
-
-	public override void _Ready()
-	{
-		_player = GetParent() as CardPlayer;
-		// Verify Hand is owned by a CardPlayer
-	}
 }
--- implementations/BasicHand.cs
+++ implementations/BasicHand.cs
@@ -4,9 +4,25 @@ using System;
 // A Hand that makes all received cards visible to the owner
 public partial class BasicHand : Hand
 {
+	protected CardPlayer _player;
+
 	public override void AddCard(Card card)
 	{
 		base.AddCard(card);
 		card.ShowToPlayer(_player.PlayerId);
+	}
+
+	public override void _Notification(int what)
+	{
+		base._Notification(what);
+		switch ((long)what)
+		{
+			case NotificationParented:
+				_player = GetParent() as CardPlayer;
+				break;
+			case NotificationUnparented:
+				_player = null;
+				break;
+		}
 	}
 }
