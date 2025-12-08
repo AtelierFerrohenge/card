93b03f9698293f0dd8cb5e8e91447a5805b08007
Atelier Ferrohenge <kai@ferrohenge.com>
2025-12-08 21:03:17 +0800

    Signal control for visuals

--- card_info.gd
+++ card_info.gd
@@ -1,11 +1,14 @@
 class_name CardInfo
 extends Node
 
+signal update_visuals(show_to: int)
+
 var group: CardGroup = null
 var group_index: int = -1
 var _shown_to: int = 0:
 	set = set_shown_to
-var _visual_node: Node = null
+var _visual_node: Node = null:
+	set = set_visual_node
 
 
 func show_to_player(id: int) -> void:
@@ -19,4 +22,13 @@ func hide_from_player(id: int) -> void:
 func set_shown_to(value: int) -> void:
 	if _shown_to != value:
 		_shown_to = value
-		_visual_node.update_visuals(_shown_to)
+		update_visuals.emit(_shown_to)
+
+
+func set_visual_node(value: Node) -> void:
+	# Check the target method exists
+	if _visual_node != null:
+		update_visuals.disconnect(_visual_node.update_visuals)
+	_visual_node = value
+	if _visual_node != null:
+		update_visuals.connect(_visual_node.update_visuals)
