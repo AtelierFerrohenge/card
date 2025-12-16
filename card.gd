a8834501ed0efa4827bc86b4858c874340ee6362
Atelier Ferrohenge <kai@ferrohenge.com>
2025-12-17 00:53:42 +0800

    Prototype visual card instantiation

--- card.gd
+++ card.gd
@@ -1,10 +1,21 @@
+@abstract
 class_name Card
 extends Node
 
 signal update_visuals(show_to: int)
 
+@export var visual_card_scene: PackedScene
+
 var _shown_to: int = 0:
-	set = set_shown_to
+	set = _set_shown_to
+
+
+func _ready() -> void:
+	# Temporarily use get_parent()
+	var visual_card_instance: Node = visual_card_scene.instantiate()
+	# Encode the requirement for visual_card_ready somewhere
+	visual_card_instance.visual_card_ready.connect(_on_visual_card_ready)
+	get_parent().add_child.call_deferred(visual_card_instance)
 
 
 func show_to_player(id: int) -> void:
@@ -15,7 +26,10 @@ func hide_from_player(id: int) -> void:
 	_shown_to &= ~(1 << id)
 
 
-func set_shown_to(value: int) -> void:
+@abstract func _on_visual_card_ready(visual_card: Node) -> void
+
+
+func _set_shown_to(value: int) -> void:
 	if _shown_to != value:
 		_shown_to = value
 		update_visuals.emit(_shown_to)
