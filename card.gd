dff8f3335db21eb65ec31094631a0b4462a83a62
Atelier Ferrohenge <kai@ferrohenge.com>
2025-12-17 18:12:00 +0800

    Update card for visual card coupling

--- card.gd
+++ card.gd
@@ -2,20 +2,19 @@
 class_name Card
 extends Node
 
-signal update_visuals(show_to: int)
-
 @export var visual_card_scene: PackedScene
 
 var _shown_to: int = 0:
 	set = _set_shown_to
 
+var _visual_card: Node
+
 
 func _ready() -> void:
+	_visual_card = visual_card_scene.instantiate()
+	_visual_card.ready.connect(_on_visual_card_ready)
 	# Temporarily use get_parent()
-	var visual_card_instance: Node = visual_card_scene.instantiate()
-	# Encode the requirement for visual_card_ready somewhere
-	visual_card_instance.visual_card_ready.connect(_on_visual_card_ready)
-	get_parent().add_child.call_deferred(visual_card_instance)
+	get_parent().add_child.call_deferred(_visual_card)
 
 
 func show_to_player(id: int) -> void:
@@ -26,10 +25,12 @@ func hide_from_player(id: int) -> void:
 	_shown_to &= ~(1 << id)
 
 
-@abstract func _on_visual_card_ready(visual_card: Node) -> void
+@abstract func _on_visual_card_ready() -> void
+
+
+@abstract func _update_shown_to_visual() -> void
 
 
 func _set_shown_to(value: int) -> void:
-	if _shown_to != value:
-		_shown_to = value
-		update_visuals.emit(_shown_to)
+	_shown_to = value
+	_update_shown_to_visual()
