2e9765b8f37fef55502fed0200fe837b7e537f7e
Atelier Ferrohenge <kai@ferrohenge.com>
2025-12-27 08:23:02 -0600

    Virtualize card group extensible functions

--- src/card_group.cpp
+++ src/card_group.cpp
@@ -1,6 +1,10 @@
 #include "card_group.h"
 
 void CardGroup::transfer_card(Card *p_card) {
+    if(GDVIRTUAL_CALL(_transfer_card, p_card)) {
+        return;
+    }
+
     // Review if get_parent() called on the card is the best way to transfer
     if(p_card->get_parent() == nullptr) {
         add_child(p_card);
@@ -9,23 +13,43 @@ void CardGroup::transfer_card(Card *p_card) {
     }
 }
 
-bool CardGroup::is_empty() {
-    return get_child_count() == 0;
+int CardGroup::get_card_count() const {
+    int card_count = 0;
+    if(GDVIRTUAL_CALL(_get_card_count, card_count)) {
+        return card_count;
+    }
+
+    return get_child_count();
 }
 
-Card *CardGroup::get_random_card() {
+Card *CardGroup::get_random_card() const {
+    Card *card = nullptr;
+    if(GDVIRTUAL_CALL(_get_random_card, card)) {
+        return card;
+    }
+
     // Review if (Card *) is the best way to cast this
     return is_empty() ? nullptr : (Card *)get_child(UtilityFunctions::randi() % get_child_count());
 }
 
 // This doesn't actually delete the card, so may need to implement that later
 void CardGroup::erase_card(Card *p_card) {
+    if(GDVIRTUAL_CALL(_erase_card, p_card)) {
+        return;
+    }
+
     remove_child(p_card);
 }
 
+bool CardGroup::is_empty() const {
+    return get_card_count() == 0;
+}
+
 void CardGroup::_bind_methods() {
-    ClassDB::bind_method(D_METHOD("transfer_card", "card"), &CardGroup::transfer_card);
     ClassDB::bind_method(D_METHOD("is_empty"), &CardGroup::is_empty);
-    ClassDB::bind_method(D_METHOD("get_random_card"), &CardGroup::get_random_card);
-    ClassDB::bind_method(D_METHOD("erase_card", "card"), &CardGroup::erase_card);
+
+    GDVIRTUAL_BIND(_transfer_card, "card");
+    GDVIRTUAL_BIND(_get_card_count);
+    GDVIRTUAL_BIND(_get_random_card);
+    GDVIRTUAL_BIND(_erase_card, "card");
 }
--- src/card_group.h
+++ src/card_group.h
@@ -2,6 +2,8 @@
 
 #include "godot_cpp/classes/node.hpp"
 
+#include <godot_cpp/core/gdvirtual.gen.inc>
+
 // Review if this is the right way to include used custom classes
 #include "card.h"
 
@@ -14,11 +16,18 @@ public:
     CardGroup() = default;
     ~CardGroup() override = default;
 
-    void transfer_card(Card *p_card);
-    bool is_empty();
-    Card *get_random_card();
-    void erase_card(Card *p_card);
+    virtual void transfer_card(Card *p_card);
+    virtual int get_card_count() const;
+    virtual Card *get_random_card() const;
+    virtual void erase_card(Card *p_card);
+
+    bool is_empty() const;
 
 protected:
     static void _bind_methods();
+
+    GDVIRTUAL1(_transfer_card, Card *)
+    GDVIRTUAL0RC(int, _get_card_count)
+    GDVIRTUAL0RC(Card *, _get_random_card)
+    GDVIRTUAL1(_erase_card, Card *)
 };
